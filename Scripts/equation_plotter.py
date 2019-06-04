ebdde3aae332ce9d45794a87648483f479e789df
Peter Robertson <peter@the-robertsons.org.uk>
2019-06-04 22:21:44 +0100

    GANs gen loss plots

--- Scripts/equation_plotter.py
+++ Scripts/equation_plotter.py
@@ -6,20 +6,21 @@ import os
 x = np.linspace(0.01, 0.99, 100)
 
 # Equations to plot
-y1 = np.log(x)
-y2 = np.log(1-x)
+y1 = np.log(1-x)
+y2 = -np.log(x)
 
 fig, ax = plt.subplots()
-ax.set_title('Loss Plots for Original GANs Loss Function')
+#ax.set_title('Loss Plots for Original GANs Loss Function')
 
-line1, = ax.plot(x, y1, label='Discriminator Loss')
-line2, = ax.plot(x, y2, label='Generator Loss')
+line1, = ax.plot(x, y1, label='Original Generator Loss')
+line2, = ax.plot(x, y2, label='Improved Generator Loss')
 
 ax.legend()
 #plt.show()
 
-filename = "original_gans_loss.png"
-save_path = os.path.join("figures", filename)
+filename = "goodfellow_gen_losses.png"
+dir_path = os.path.dirname(os.path.realpath(__file__))
+save_path = os.path.join(dir_path, "figures", filename)
 
 if not os.path.exists("figures"):
     os.makedirs("figures")
