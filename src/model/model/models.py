60c880e96bb1b5477dfc1603d04569191da7c2ba
Peter Robertson <peter@the-robertsons.org.uk>
2019-07-31 11:37:08 +0100

    removed unused models. Updated mfcc-shapes gan model

--- src/model/model/models.py
+++ src/model/model/models.py
@@ -3,218 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 
-class Generator_MNIST(BaseModel):
-    def __init__(self, z_dim=100):
-        super().__init__()
-
-        self.convt1 = nn.ConvTranspose2d(z_dim, 256, kernel_size=4, bias=False)
-        self.bn1 = nn.BatchNorm2d(256)
-        self.relu1 = nn.ReLU()
-
-        self.convt2 = nn.ConvTranspose2d(256, 128, kernel_size=3, bias=False)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.relu2 = nn.ReLU()
-
-        self.convt3 = nn.ConvTranspose2d(128, 64, kernel_size=3, bias=False)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.relu3 = nn.ReLU()
-
-        self.convt4 = nn.ConvTranspose2d(64, 64, kernel_size=4, bias=False)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.relu4 = nn.ReLU()
-
-        self.convt5 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, bias=False)
-        self.bn5 = nn.BatchNorm2d(32)
-        self.relu5 = nn.ReLU()
-
-        self.convt6 = nn.ConvTranspose2d(32, 16, kernel_size=3, bias=False)
-        self.bn6 = nn.BatchNorm2d(16)
-        self.relu6 = nn.ReLU()
-
-        self.convt7 = nn.ConvTranspose2d(16, 1, kernel_size=3, bias=False)
-        self.tanh7 = nn.Tanh()
-
-
-    def forward(self, x):
-        x = x.view(x.size(0), x.size(1), 1, 1)
-        x = self.relu1(self.bn1(self.convt1(x)))
-        x = self.relu2(self.bn2(self.convt2(x)))
-        x = self.relu3(self.bn3(self.convt3(x)))
-        x = self.relu4(self.bn4(self.convt4(x)))
-        x = self.relu5(self.bn5(self.convt5(x)))
-        x = self.relu6(self.bn6(self.convt6(x)))
-        out = self.tanh7(self.convt7(x))
-        return out
-
-class Discriminator_MNIST(BaseModel):
-    def __init__(self):
-        super().__init__()
-
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, bias=False)
-        self.leaky_relu1 = nn.LeakyReLU()
-        self.drop1 = nn.Dropout2d(0.2)
-
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, bias=False)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.leaky_relu2 = nn.LeakyReLU()
-        self.drop2 = nn.Dropout2d(0.2)
-
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=4, stride=2, bias=False)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.leaky_relu3 = nn.LeakyReLU()
-        self.drop3 = nn.Dropout2d(0.2)
-
-        self.conv4 = nn.Conv2d(64, 64, kernel_size=4, bias=False)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.leaky_relu4 = nn.LeakyReLU()
-        self.drop4 = nn.Dropout2d(0.2)
-
-        self.conv5 = nn.Conv2d(64, 64, kernel_size=3, bias=False)
-        self.bn5 = nn.BatchNorm2d(64)
-        self.leaky_relu5 = nn.LeakyReLU()
-        self.drop5 = nn.Dropout2d(0.2)
-
-        self.conv6 = nn.Conv2d(64, 32, kernel_size=3, bias=False)
-        self.bn6 = nn.BatchNorm2d(32)
-        self.leaky_relu6 = nn.LeakyReLU()
-        self.drop6 = nn.Dropout2d(0.2)
-
-        self.conv7 = nn.Conv2d(32, 1, kernel_size=4, bias=False)
-        self.sigmoid7 = nn.Sigmoid()
-
-    def forward(self, x):
-        x = self.drop1(self.leaky_relu1(self.conv1(x)))
-        x = self.drop2(self.leaky_relu2(self.bn2(self.conv2(x))))
-        x = self.drop3(self.leaky_relu3(self.bn3(self.conv3(x))))
-        x = self.drop4(self.leaky_relu4(self.bn4(self.conv4(x))))
-        x = self.drop5(self.leaky_relu5(self.bn5(self.conv5(x))))
-        x = self.drop6(self.leaky_relu6(self.bn6(self.conv6(x))))
-        out = self.sigmoid7(self.conv7(x))
-        out = torch.squeeze(out)
-        return out
-
-class Critic_MNIST(BaseModel):
-    def __init__(self):
-        super().__init__()
-
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3)
-        self.leaky_relu1 = nn.LeakyReLU(0.2)
-
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
-        self.leaky_relu2 = nn.LeakyReLU(0.2)
-
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.leaky_relu3 = nn.LeakyReLU(0.2)
-
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=4)
-        self.leaky_relu4 = nn.LeakyReLU(0.2)
-
-        self.conv5 = nn.Conv2d(128, 256, kernel_size=3)
-        self.leaky_relu5 = nn.LeakyReLU(0.2)
-
-        self.conv6 = nn.Conv2d(256, 64, kernel_size=3)
-        self.leaky_relu6 = nn.LeakyReLU(0.2)
-
-        self.conv7 = nn.Conv2d(64, 1, kernel_size=4)
-        self.linear = nn.Linear(1, 1)
-
-    def forward(self, x):
-        x = self.leaky_relu1(self.conv1(x))
-        x = self.leaky_relu2(self.conv2(x))
-        x = self.leaky_relu3(self.conv3(x))
-        x = self.leaky_relu4(self.conv4(x))
-        x = self.leaky_relu5(self.conv5(x))
-        x = self.leaky_relu6(self.conv6(x))
-        out = self.linear(self.conv7(x))
-        out = torch.squeeze(out)
-        return out
-
-class Cond_Generator_MNIST(BaseModel):
-    def __init__(self, z_dim=100, n_labels=10):
-        super().__init__()
-
-        in_dim = z_dim + n_labels
-
-        self.convt1 = nn.ConvTranspose2d(in_dim, 256, kernel_size=4)
-        self.bn1 = nn.BatchNorm2d(256)
-        self.relu1 = nn.ReLU()
-
-        self.convt2 = nn.ConvTranspose2d(256, 128, kernel_size=3)
-        self.bn2 = nn.BatchNorm2d(128)
-        self.relu2 = nn.ReLU()
-
-        self.convt3 = nn.ConvTranspose2d(128, 64, kernel_size=3)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.relu3 = nn.ReLU()
-
-        self.convt4 = nn.ConvTranspose2d(64, 64, kernel_size=4)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.relu4 = nn.ReLU()
-
-        self.convt5 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2)
-        self.bn5 = nn.BatchNorm2d(32)
-        self.relu5 = nn.ReLU()
-
-        self.convt6 = nn.ConvTranspose2d(32, 16, kernel_size=3)
-        self.bn6 = nn.BatchNorm2d(16)
-        self.relu6 = nn.ReLU()
-
-        self.convt7 = nn.ConvTranspose2d(16, 1, kernel_size=3)
-        self.tanh7 = nn.Tanh()
-
-    def forward(self, z, c):
-        x = torch.cat((z, c), dim=1)
-        x = x.view(x.size(0), x.size(1), 1, 1)
-        x = self.relu1(self.bn1(self.convt1(x)))
-        x = self.relu2(self.bn2(self.convt2(x)))
-        x = self.relu3(self.bn3(self.convt3(x)))
-        x = self.relu4(self.bn4(self.convt4(x)))
-        x = self.relu5(self.bn5(self.convt5(x)))
-        x = self.relu6(self.bn6(self.convt6(x)))
-        out = self.tanh7(self.convt7(x))
-        return out
-
-class Cond_Critic_MNIST(BaseModel):
-    def __init__(self, in_channels, n_labels):
-        super().__init__()
-
-        in_dim = in_channels + n_labels
-
-        self.conv1 = nn.Conv2d(in_dim, 16, kernel_size=3, bias=False)
-        self.leaky_relu1 = nn.LeakyReLU()
-
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, bias=False)
-        self.leaky_relu2 = nn.LeakyReLU()
-
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=4, stride=2, bias=False)
-        self.leaky_relu3 = nn.LeakyReLU()
-
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=4, bias=False)
-        self.leaky_relu4 = nn.LeakyReLU()
-
-        self.conv5 = nn.Conv2d(128, 256, kernel_size=3, bias=False)
-        self.leaky_relu5 = nn.LeakyReLU()
-
-        self.conv6 = nn.Conv2d(256, 64, kernel_size=3, bias=False)
-        self.leaky_relu6 = nn.LeakyReLU()
-
-        self.conv7 = nn.Conv2d(64, 1, kernel_size=4, bias=False)
-        self.linear = nn.Linear(1, 1, bias=False)
-
-    def forward(self, x, c):
-        c = c.unsqueeze(-1).unsqueeze(-1)
-        c = c.expand((c.size(0), c.size(1), x.size(2), x.size(3)))
-        x = torch.cat((x, c), dim=1)
-        x = self.leaky_relu1(self.conv1(x))
-        x = self.leaky_relu2(self.conv2(x))
-        x = self.leaky_relu3(self.conv3(x))
-        x = self.leaky_relu4(self.conv4(x))
-        x = self.leaky_relu5(self.conv5(x))
-        x = self.leaky_relu6(self.conv6(x))
-        out = self.linear(self.conv7(x))
-        out = torch.squeeze(out)
-        return out
-
 class Voca_Shape_Generator(BaseModel):
     def __init__(self, z_dim=10, n_labels=1):
         super().__init__()
@@ -336,7 +124,7 @@ class Voca_Shape_Critic(BaseModel):
         return out
 
 class MFCC_Shape_Gen(BaseModel):
-    def __init__(self, z_dim=10, shapes_dim=5):
+    def __init__(self, z_dim, shapes_dim):
         super().__init__()
 
         in_dim = z_dim + 1
@@ -374,6 +162,7 @@ class MFCC_Shape_Gen(BaseModel):
 
         self.conv10 = nn.Conv2d(16, shapes_dim, kernel_size=(4,3), 
                                                 padding=(0,1))
+        self.tanh10 = nn.Tanh()
 
 
     def forward(self, noise, mfcc):
@@ -387,7 +176,7 @@ class MFCC_Shape_Gen(BaseModel):
         x = self.relu7(self.conv7(x))
         x = self.relu8(self.conv8(x))
         x = self.relu9(self.conv9(x))
-        x = self.conv10(x)
+        x = self.tanh10(self.conv10(x))
         return x
 
 class MFCC_Shape_Critic(BaseModel):
@@ -396,34 +185,31 @@ class MFCC_Shape_Critic(BaseModel):
 
         in_dim = shapes_dim + 1
 
-        # This could cause issues. 
-        # If in_dim is less than 32, we shrink the channels 
-        # before expanding them
-        self.conv1 = nn.Conv2d(in_dim, 32, kernel_size=3)
+        self.conv1 = nn.Conv2d(in_dim, 64, kernel_size=(4,1), stride=(2,1))
         self.lrelu1 = nn.LeakyReLU(0.2)
 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=(4,1), stride=(2,1))
         self.lrelu2 = nn.LeakyReLU(0.2)
 
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=(3,1), stride=(2,1))
         self.lrelu3 = nn.LeakyReLU(0.2)
 
-        self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.conv4 = nn.Conv2d(256, 256, kernel_size=(3,1), stride=(2,1))
         self.lrelu4 = nn.LeakyReLU(0.2)
 
-        self.conv5 = nn.Conv2d(256, 256, kernel_size=3)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=(2,1))
         self.lrelu5 = nn.LeakyReLU(0.2)
 
-        self.conv6 = nn.Conv2d(256, 256, kernel_size=(4,3), stride=2)
+        self.conv6 = nn.Conv1d(256, 256, kernel_size=5, stride=2)
         self.lrelu6 = nn.LeakyReLU(0.2)
 
-        self.conv7 = nn.Conv2d(256, 128, kernel_size=3)
+        self.conv7 = nn.Conv1d(256, 256, kernel_size=4, stride=2)
         self.lrelu7 = nn.LeakyReLU(0.2)
 
-        self.conv8 = nn.Conv2d(128, 64, kernel_size=3)
+        self.conv8 = nn.Conv1d(256, 256, kernel_size=3, stride=2)
         self.lrelu8 = nn.LeakyReLU(0.2)
 
-        self.conv9 = nn.Conv2d(64, 1, kernel_size=(4,3))
+        self.conv9 = nn.Conv1d(256, 256, kernel_size=4)
         self.lin9 = nn.Linear(1, 1)
 
     
@@ -440,6 +226,7 @@ class MFCC_Shape_Critic(BaseModel):
         x = self.lrelu3(self.conv3(x))
         x = self.lrelu4(self.conv4(x))
         x = self.lrelu5(self.conv5(x))
+        x = x.squeeze(2)
         x = self.lrelu6(self.conv6(x))
         x = self.lrelu7(self.conv7(x))
         x = self.lrelu8(self.conv8(x))
