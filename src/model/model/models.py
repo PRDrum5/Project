27117e77d906d05e1992302542a28e6342aea14e
Ubuntu <peter@Project-classification.g2djyb1rcypu3kz02uuozh4nee.ax.internal.cloudapp.net>
2019-08-02 15:47:59 +0000

    improved tuning on lrw classifier

--- src/model/model/models.py
+++ src/model/model/models.py
@@ -320,38 +320,36 @@ class Lrw_Shape_Classifier(BaseModel):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Conv1d(4, 16, kernel_size=3)
-        self.bn1 = nn.BatchNorm1d(16)
+        self.conv1 = nn.Conv1d(4, 32, kernel_size=3)
+        self.bn1 = nn.BatchNorm1d(32)
         self.relu1 = nn.ReLU()
-        self.dropout1 = nn.Dropout(0.2)
+        self.drop1 = nn.Dropout(0.05)
 
-        self.conv2 = nn.Conv1d(16, 32, kernel_size=3)
-        self.bn2 = nn.BatchNorm1d(32)
+        self.conv2 = nn.Conv1d(32, 64, kernel_size=3)
+        self.bn2 = nn.BatchNorm1d(64)
         self.relu2 = nn.ReLU()
-        self.dropout2 = nn.Dropout(0.2)
+        self.drop2 = nn.Dropout(0.05)
 
-        self.conv3 = nn.Conv1d(32, 64, kernel_size=3)
-        self.bn3 = nn.BatchNorm1d(64)
+        self.conv3 = nn.Conv1d(64, 128, kernel_size=3)
+        self.bn3 = nn.BatchNorm1d(128)
         self.relu3 = nn.ReLU()
         self.max_pool3 = nn.MaxPool1d(kernel_size=3, padding=1, stride=2)
-        self.dropout3 = nn.Dropout(0.2)
 
-        self.conv4 = nn.Conv1d(64, 128, kernel_size=3)
-        self.bn4 = nn.BatchNorm1d(128)
+        self.conv4 = nn.Conv1d(128, 256, kernel_size=3)
+        self.bn4 = nn.BatchNorm1d(256)
         self.relu4 = nn.ReLU()
-        self.dropout4 = nn.Dropout(0.2)
+        self.drop4 = nn.Dropout(0.05)
 
-        self.conv5 = nn.Conv1d(128, 256, kernel_size=3)
-        self.bn5 = nn.BatchNorm1d(256)
+        self.conv5 = nn.Conv1d(256, 512, kernel_size=3)
+        self.bn5 = nn.BatchNorm1d(512)
         self.relu5 = nn.ReLU()
         self.max_pool5 = nn.MaxPool1d(kernel_size=3, padding=1, stride=2)
-        self.dropout5 = nn.Dropout(0.2)
 
-        self.conv6 = nn.Conv1d(256, 128, kernel_size=3)
+        self.conv6 = nn.Conv1d(512, 256, kernel_size=3)
         self.relu6 = nn.ReLU()
-        self.dropout6 = nn.Dropout(0.2)
+        self.drop6 = nn.Dropout(0.05)
 
-        self.lin7 = nn.Linear(768, 500)
+        self.lin7 = nn.Linear(1536, 500)
         self.softmax7 = nn.Softmax()
 
     
@@ -362,16 +360,20 @@ class Lrw_Shape_Classifier(BaseModel):
         """
         batch_size = shapes.size(0)
 
-        x = self.dropout1(self.relu1(self.bn1(self.conv1(shapes))))
-        x = self.dropout2(self.relu2(self.bn2(self.conv2(x))))
+        x = self.relu1(self.bn1(self.conv1(shapes)))
+        x = self.drop1(x)
+        x = self.relu2(self.bn2(self.conv2(x)))
+        x = self.drop2(x)
         x = self.relu3(self.bn3(self.conv3(x)))
-        x = self.dropout3(self.max_pool3(x))
+        x = self.max_pool3(x)
 
-        x = self.dropout4(self.relu4(self.bn4(self.conv4(x))))
+        x = self.relu4(self.bn4(self.conv4(x)))
+        x = self.drop4(x)
         x = self.relu5(self.bn5(self.conv5(x)))
-        x = self.dropout5(self.max_pool5(x))
+        x = self.max_pool5(x)
 
-        x = self.dropout6(self.relu6(self.conv6(x)))
+        x = self.relu6(self.conv6(x))
+        x = self.drop6(x)
         x = x.view(batch_size, -1)
         x = self.softmax7(self.lin7(x))
 
